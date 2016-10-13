54f3c5b0884904785d5de3cc31b57c5c111ba1a0
Napat Wannachai <napatwannachai@hotmail.com>
2016-10-13 19:53:50 +0700

    Add class Captcha

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,19 +1,35 @@
 var operandtostring = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];
 var operandtostring2 = ["0","1","2","3","4","5","6","7","8","9"];
 
-var operator = ["+","-"];
 function Leftoperand (int pattern, int leftoperand) {
-    this.ltostring = function() {
-      if(pattern === 1) {return operandtostring[leftoperand-1]}
-      else {return operandtostring2[leftoperand-1]}
-    }
+  this.pattern = pattern;
+  this.leftoperand = leftoperand;
+  this.ltostring = function() {
+    if(pattern === 1) {return operandtostring[this.leftoperand-1]}
+    else {return operandtostring2[this.leftoperand-1]}
+  }
 }
 function Rightoperand (int pattern, int rightoperand) {
-    this.rtostring = function() {
-      if(pattern === 2) {return operandtostring[righttoperand-1]}
-      else {return operandtostring2[rightoperand-1]}
-    }
+  this.pattern = pattern;
+  this.rightoperand = rightoperand;
+  this.rtostring = function() {
+    if(pattern === 2) {return operandtostring[this.righttoperand-1]}
+    else {return operandtostring2[this.rightoperand-1]}
+  }
 }
+function Operator (int operator)
+{
+  this.otostring = function(){ }
+}
+function Captcha(int pattern ,int operator, int leftoperand ,int rightoperand) {
+  var l = new Leftoperand(pattern,leftoperand);
+  var r = new Rightoperand(pattern,rightoperand)
+  var o = new
+  this.generate = function(){
+    return l.ltostring() + " " + o.otosting()+ " " + r.rtostring();
+  }
+}
+
 
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
