19fb83e360ab72c625a5e5e8b3837a472f3e81a6
Supawit Uttra <supawit.ut@hotmail.com>
2016-10-14 16:38:30 +0700

    Edit Pattern and test case (Switch)

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -3,14 +3,14 @@ var operandtostring2 = ["0","1","2","3","4","5","6","7","8","9"];
 
 function Leftoperand (pattern, leftoperand) {
   this.ltostring = function() {
-    if(pattern === 1) {return operandtostring[leftoperand];}
-    else {return operandtostring2[leftoperand];}
+    if(pattern === 1) {return operandtostring2[leftoperand];}
+    else {return operandtostring[leftoperand];}
   }
 }
 function Rightoperand (pattern,rightoperand) {
   this.rtostring = function() {
-    if(pattern === 2) {return operandtostring[rightoperand];}
-    else {return operandtostring2[rightoperand];}
+    if(pattern === 2) {return operandtostring2[rightoperand];}
+    else {return operandtostring[rightoperand];}
   }
 }
 function Operator (operator)
@@ -34,28 +34,45 @@ function CreateCaptcha(pattern ,operator,leftoperand,rightoperand) {
     return new Captcha(pattern ,operator,leftoperand,rightoperand);
 }
 
+//Pattern1+
 describe("CaptchaApp", function() {
-      it('should generate "9 - Six" when CreteCaptcha(2,2,9,6)', function() {
-        expect(new CreateCaptcha(2,2,9,6).generate()).toEqual('9 - Six');
+      it('should generate "1 + One" when CreteCaptcha(1,1,1,1)', function() {
+        expect(new CreateCaptcha(1,1,1,1).generate()).toEqual('1 + One');
+      })
+});
+
+//Pattern1+
+
+
+
+
+
+
+//Pattern2+
+describe("CaptchaApp", function() {
+      it('should generate "Seven + 4" when CreteCaptcha(2,1,7,4)', function() {
+        expect(new CreateCaptcha(2,1,7,4).generate()).toEqual('Seven + 4');
       })
 });
 describe("CaptchaApp", function() {
-      it('should generate "One + 1" when CreteCaptcha(1,1,1,1)', function() {
-        expect(new CreateCaptcha(1,1,1,1).generate()).toEqual('One + 1');
+      it('should generate "Three + 2" when CreteCaptcha(2,1,3,2)', function() {
+        expect(new CreateCaptcha(2,1,3,2).generate()).toEqual('Three + 2');
       })
 });
 describe("CaptchaApp", function() {
-      it('should generate "8 - Seven" when CreteCaptcha(2,2,8,7)', function() {
-        expect(new CreateCaptcha(2,2,8,7).generate()).toEqual('8 - Seven');
+      it('should generate "Two + 1" when CreteCaptcha(2,1,2,1)', function() {
+        expect(new CreateCaptcha(2,1,2,1).generate()).toEqual('Two + 1');
+      })
+});
+
+//Pattern2-
+describe("CaptchaApp", function() {
+      it('should generate "Nine - 6" when CreteCaptcha(2,2,9,6)', function() {
+        expect(new CreateCaptcha(2,2,9,6).generate()).toEqual('Nine - 6');
       })
 });
 describe("CaptchaApp", function() {
-      it('should generate "7 + Four" when CreteCaptcha(2,1,7,4)', function() {
-        expect(new CreateCaptcha(2,1,7,4).generate()).toEqual('7 + Four');
-      })
-});
-describe("CaptchaApp", function() {
-      it('should generate "3 + Two" when CreteCaptcha(2,1,3,2)', function() {
-        expect(new CreateCaptcha(2,1,7,4).generate()).toEqual('7 + Four');
+      it('should generate "Eight - 7" when CreteCaptcha(2,2,8,7)', function() {
+        expect(new CreateCaptcha(2,2,8,7).generate()).toEqual('Eight - 7');
       })
 });
