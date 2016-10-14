6b54be441f1432fc5e846e5a5937bbf2a7c29ca5
Napat Wannachai <napatwannachai@hotmail.com>
2016-10-14 14:31:06 +0700

    Fixed Syntax error

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,23 +1,19 @@
 var operandtostring = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];
 var operandtostring2 = ["0","1","2","3","4","5","6","7","8","9"];
 
-function Leftoperand (int pattern, int leftoperand) {
-  this.pattern = pattern;
-  this.leftoperand = leftoperand;
+function Leftoperand (pattern, leftoperand) {
   this.ltostring = function() {
-    if(pattern === 1) {return operandtostring[this.leftoperand]}
-    else {return operandtostring2[this.leftoperand]}
+    if(pattern === 1) {return operandtostring[leftoperand];}
+    else {return operandtostring2[leftoperand];}
   }
 }
-function Rightoperand (int pattern, int rightoperand) {
-  this.pattern = pattern;
-  this.rightoperand = rightoperand;
+function Rightoperand (pattern,rightoperand) {
   this.rtostring = function() {
-    if(pattern === 2) {return operandtostring[this.righttoperand]}
-    else {return operandtostring2[this.rightoperand]}
+    if(pattern === 2) {return operandtostring[righttoperand]}
+    else {return operandtostring2[rightoperand];}
   }
 }
-function Operator (int operator)
+function Operator (operator)
 {
   this.otostring = function(){
       if(operator === 1){ return ' + ';  }
@@ -26,11 +22,11 @@ function Operator (int operator)
       else if(operator === 4){ return ' / ';  }
     }
 }
-function Captcha(int pattern ,int operator, int leftoperand ,int rightoperand) {
+function Captcha(pattern ,operator,leftoperand,rightoperand) {
   var l = new Leftoperand(pattern,leftoperand);
   var r = new Rightoperand(pattern,rightoperand);
   var o = new Operator(operator);
   this.generate = function(){
-    return l.ltostring() + o.otosting() + r.rtostring();
+    return l.ltostring() + o.otostring() + r.rtostring();
   }
 }
